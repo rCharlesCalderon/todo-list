a86480c8b36494eb274ccca7e03c0d5910717e32
Ruben Calderon <Rcaldeon0907@gmail.com>
2023-04-30 00:47:31 -0700

    .

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
-import _ from "lodash";
+import _, { indexOf } from "lodash";
 import "./style.css";
 import { todo } from "./todo";
 import { projectCreate } from "./projectDOM";
+let projectArray = [];
+let button = document.querySelector(".add-project");
+
+button.addEventListener("click", () => {
+  let projectContainer = document.querySelector(".projects");
+  let projectCard = document.createElement("div");
+  projectCard.classList.add("project-card");
+  projectContainer.appendChild(projectCard);
+  projectArray.push(projectCard);
+  projectArray.forEach((card) => {
+    projectCard.dataset = projectArray.indexOf(card);
+  });
+});
--- src/projectDOM.js
+++ src/projectDOM.js
@@ -1,14 +1,3 @@
 import { forEach } from "lodash";
 
-export function projectCreate() {
-  let projectCard = document.createElement("div");
-  projectCard.classList.add("project-card");
-  let projectContainer = document.querySelector(".projects");
-  let button = document.querySelector(".add-project");
-  projectContainer.insertBefore(projectCard, button);
-  let grabProjectCard = document.querySelector(".project-card");
-  grabProjectCard.forEach((card) => {
-    console.log(card);
-  });
- 
-}
+
